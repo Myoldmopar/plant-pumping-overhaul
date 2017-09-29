58f115991dfaacea9c4ea41df8ab79d225d422b7
Edwin Lee <leeed2001@gmail.com>
2017-09-29 15:27:08 -0500

    Add runner script to handle different test types

---
+++ runner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $1 == "test" ]; then
+  bundle exec ruby tests/ts_all_tests.rb && make -C report pdf
+  exit $?
+elif [ $1 == "rubocop" ]; then
+  bundle exec rubocop -D
+  exit $?
+fi
