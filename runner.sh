47d30444e9c99218868f7bc2709da92fe2166923
Edwin Lee <leeed2001@gmail.com>
2017-09-30 07:23:59 -0500

    Only make report in build job

--- runner.sh
+++ runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $1 == "tests" ]; then
-  bundle exec ruby tests/ts_all_tests.rb && make -C report pdf
+  bundle exec ruby tests/ts_all_tests.rb
   exit $?
 elif [ $1 == "rubocop" ]; then
   bundle exec rubocop -D
@@ -12,5 +12,6 @@ elif [ $1 == "build" ]; then
   wget https://github.com/NREL/EnergyPlus/releases/download/v8.8.0/EnergyPlus-8.8.0-7c3bbe4830-Linux-x86_64.tar.gz
   tar -xvf EnergyPlus-8.8.0-7c3bbe4830-Linux-x86_64.tar.gz
   bundle exec ruby lib/build_configurations.rb
+  make -C report pdf
   exit $?
 fi
