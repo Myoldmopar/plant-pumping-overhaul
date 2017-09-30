354d4b6d9be341aaad0077df59c89ba164fb5b92
Edwin Lee <leeed2001@gmail.com>
2017-09-29 21:09:33 -0500

    And do things in the right order

--- runner.sh
+++ runner.sh
@@ -9,8 +9,8 @@ elif [ $1 == "rubocop" ]; then
 elif [ $1 == "build" ]; then
   wget https://github.com/NREL/OpenStudio/releases/download/v2.2.2/OpenStudio-2.2.2.ebdeaa44f8-Linux.deb
   sudo gdebi -n OpenStudio-2.2.2.ebdeaa44f8-Linux.deb
-  bundle exec ruby lib/build_configurations.rb
   wget https://github.com/NREL/EnergyPlus/releases/download/v8.8.0/EnergyPlus-8.8.0-7c3bbe4830-Linux-x86_64.tar.gz
   tar -xvf EnergyPlus-8.8.0-7c3bbe4830-Linux-x86_64.tar.gz
+  bundle exec ruby lib/build_configurations.rb
   exit $?
 fi
